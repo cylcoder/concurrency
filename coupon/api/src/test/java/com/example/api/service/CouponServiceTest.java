a3cb1fe73cc5f08ee3db894c95cced7c0c6dd79b
cylcoder <cylcoder@gmail.com>
2025-07-09 12:53:00 +0900

    test: add concurrency test to reproduce coupon over-issuance issue

--- coupon/api/src/test/java/com/example/api/service/CouponServiceTest.java
+++ coupon/api/src/test/java/com/example/api/service/CouponServiceTest.java
@@ -4,6 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.api.repository.CouponRepository;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,9 +23,33 @@ class CouponServiceTest {
   private CouponRepository couponRepository;
 
   @Test
-  void applyCoupon() {
+  @DisplayName("한명의 사용자가 쿠폰을 응모할 때 정상 발급된다.")
+  void singleApply() {
     couponService.applyCoupon(1L);
     assertThat(couponRepository.count()).isOne();
+  }
+
+  @Test
+  @DisplayName("")
+  void concurrentApply() throws InterruptedException {
+    int threadCount = 1000;
+    ExecutorService executorService = Executors.newFixedThreadPool(32);
+    CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+    for (int i = 0; i < threadCount; i++) {
+      long userId = i + 1;
+      executorService.submit(() -> {
+        try {
+          couponService.applyCoupon(userId);
+        } finally {
+           countDownLatch.countDown();
+        }
+      });
+    }
+
+    countDownLatch.await();
+
+    assertThat(couponRepository.count()).isGreaterThan(100);
   }
 
 }
