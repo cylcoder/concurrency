05f2768e6d532638596a3964dbdf237740319566
cylcoder <cylcoder@gmail.com>
2025-07-13 20:38:02 +0900

    feat: implement Kafka producer for coupon creation

--- coupon/api/src/main/java/com/example/api/service/CouponService.java
+++ coupon/api/src/main/java/com/example/api/service/CouponService.java
@@ -1,6 +1,7 @@
 package com.example.api.service;
 
 import com.example.api.domain.Coupon;
+import com.example.api.producer.CouponProducer;
 import com.example.api.repository.CouponRepository;
 import com.example.api.repository.RedisCouponRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,15 +13,18 @@ public class CouponService {
   
   private final CouponRepository couponRepository;
   private final RedisCouponRepository redisCouponRepository;
+  private final CouponProducer couponProducer;
 
   public void applyCoupon(Long userId) {
 //    long count = couponRepository.count();
 
     long count = redisCouponRepository.incrementCouponCount();
 
-    if (count <= 100) {
+    /*if (count <= 100) {
       couponRepository.save(new Coupon(userId));
-    }
+    }*/
+
+    couponProducer.createCoupon(userId);
   }
 
 }
